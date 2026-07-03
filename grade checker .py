7c50d4be844cbce8dc2b37f3c1d10a491e6b36b4
chinorutendo-sudo <chinorutendo@gmail.com>
2026-07-03 05:23:43 -0700

    Add files via upload

---
+++ grade checker .py
@@ -0,0 +1,8 @@
+mark = int(input("Enter your marks: "))
+if mark >= 90:
+    print("grade A")
+elif mark >= 75:
+    print("grade B")
+elif mark >= 50:
+    print("grade C")
+elif mark >= 40: print("fail")
